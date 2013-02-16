4def874005830b654283affb938c4269901a3421
dl8.nir@gmail.com <dl8.nir@gmail.com>
2013-02-16 16:25:09 +0200

    added documentation from header, added step() & countNeighbors(), started operator<<

--- Board.cpp
+++ Board.cpp
@@ -1,18 +1,97 @@
 #include "Board.h"
+#include <algorithm>
 
-using namespace Life;
-using Matrix::Matrix;
+namespace Life {
+	using std::max;
+	using std::min;
+	/**
+	 * builds a square board
+	 * @param size size of the board
+	 */
+	Board::Board (const int size) : Board (size, size) {
+	}
 
-Board::Board (const int size) : Board (size, size) {
-}
+	/**
+	 * builds a w*h board
+	 * @param h height
+	 * @param w width
+	 */
+	Board::Board (const int h, const int w) : board (h, w) {
+	}
 
-Board::Board (const int h, const int w) : board (h, w) {
-}
+	Board::~Board() {
+	}
 
-const bool &Board::operator() (const int r, const int c) const {
-	return board (r, c);
-}
+	/**
+	 * const cell access
+	 * @param r row
+	 * @param c column
+	 * @return cell at r,c (const)
+	 */
+	const bool &Board::operator() (const int r, const int c) const {
+		return board (r, c);
+	}
 
-bool &Board::operator() (const int r, const int c) {
-	return board (r, c);
+	/**
+	 * cell access
+	 * @param r row
+	 * @param c column
+	 * @return cell at r,c
+	 */
+	bool &Board::operator() (const int r, const int c) {
+		return board (r, c);
+	}
+
+	/**
+	 * performs a single step
+	 * @return a reference to the board after the step
+	 */
+	Board &Board::step() {
+		Matrix<bool> result (board);
+		for (int i = 0; i < board.getHeight(); i++) {
+			for (int j = 0; j < board.getWidth(); j++) {
+				int neighbors = countNeighbors (i, j);
+				switch (neighbors) {
+				case 2:
+					break;
+				case 3:
+					if (!board (i, j)) {
+						result (i, j) = true;
+					}
+					break;
+				default:
+					result (i, j) = false;
+				}
+			}
+		}
+		board = result;
+		return *this;
+	}
+
+	/**
+	 * counts number of neighbors of the given cell
+	 * @param r row
+	 * @param c column
+	 * @return the number of neighbors
+	 */
+	int Board::countNeighbors (const int r, const int c) {
+		int count = 0;
+		for (int i = max (r - 1, 0); i <= min (r + 1, board.getHeight() - 1); i++) {
+			for (int j = max (c - 1, 0); j <= min (c + 1, board.getWidth() - 1); j++) {
+				if ( (i == r) && (j == c)) {
+					continue;
+				}
+				if (board (i, j)) {
+					count++;
+				}
+			}
+		}
+		return count;
+	}
+
+	ostream &operator<< (ostream &os, const Board &b) {
+		os<<b;
+		return os;
+	}
+
 }
